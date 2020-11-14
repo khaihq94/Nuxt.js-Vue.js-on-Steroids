c98713453b9ec086a7015195a8c0c4440c590405
hqkhai <hwk94uit@gmail.com>
2020-11-14 15:13:15 +0700

    Improving the App

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <button @click="$router.push('/admin/new-post')">Create Post</button>
+      <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
     </section>
     <section class="existing-posts">
       <h1>Existing post</h1>
-      <PostList isAdmin="true"/>
+      <PostList isAdmin/>
     </section>
   </div>
 </template>
@@ -13,10 +13,12 @@
 <script lang="ts">
 import Vue from "vue";
 import PostList from "@/components/Posts/PostList.vue";
+import AppButton from '@/components/UI/AppButton.vue'
 
 export default Vue.extend({
   components: {
-    PostList
+    PostList,
+    AppButton
   }
 });
 </script>
